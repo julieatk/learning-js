d381b1dca3f6165d43757f95dd2e3dd79078e510
juliatk <juia_atkeson25@milton.edu>
2025-03-06 09:04:40 -0500

    arrow function and half of array question

--- learning.js
+++ learning.js
@@ -37,3 +37,42 @@ function arithmetic(p1, p2) {
 
 //   https://www.w3schools.com/js/js_functions.asp
 
+
+console.log("arrow");
+
+
+arrow = (val, num) => arithmetic(val, num);
+let val = getRandomInt(100);
+let num = getRandomInt(100);
+
+console.log(val);
+console.log(num);
+
+console.log(arrow(val, num));
+
+const songs = [];
+songs[0]= "cruel summer";
+songs[1]= "lover";
+songs[2]= "the man";
+songs[3]= "the archer";
+songs[4]= "i think he knows";
+songs[5]= "miss americana";
+songs[6]= "paper rings";
+songs[7]= "cornelia street";
+songs[8]= "death by a thousand cuts";
+songs[9]= "london boy";
+songs[10]= "you need to calm down";
+songs[11]= "false god";
+songs[12]= "afterglow";
+songs[13]= "it's nice to have a friend";
+songs[14]= "daylight";
+
+let size = songs.length;
+let first = songs.at(0);
+let last = songs.at(14);
+
+console.log("size of array is " + size);
+console.log("first song is " + first);
+console.log("last song is " + last);
+
+// https://www.w3schools.com/js/js_arrays.asp
