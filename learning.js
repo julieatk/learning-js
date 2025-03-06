05a18582ac6c0528dd93b9f780941453639c7526
juliatk <juia_atkeson25@milton.edu>
2025-03-06 08:11:32 -0500

    question 2 and 3

--- learning.js
+++ learning.js
@@ -3,7 +3,7 @@ function getRandomInt(max) {
   }
   
 
- var number = getRandomInt(500);
+ var number = getRandomInt(100);
  console.log(number);
  //   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
@@ -16,8 +16,24 @@ else{ console.log("number is not divisible by 7 and 9");}
 
 // https://stackoverflow.com/questions/66305058/check-if-number-is-divisible-by-another-number-in-js
 
-function temprerature(temperature) {
-    return temperature ? "50" : "40";
-}
+let temp = number;
 
-console.log(getFee(true));
+let res = (temp < 60) ? "too cold" :
+          (temp > 90) ? "too hot" :
+          (60 < temp < 90) ? "perfect" : "Excellent";
+
+console.log(res);
+
+// https://www.geeksforgeeks.org/javascript-ternary-operator/
+
+let p1 = 4
+let p2 = 8
+
+function arithmetic(p1, p2) {
+    return p1 + p2;
+  }
+
+  console.log( arithmetic(p1, p2));
+
+//   https://www.w3schools.com/js/js_functions.asp
+
